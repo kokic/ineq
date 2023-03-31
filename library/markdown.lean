b036e74dbd29c9178d631c7d2505aa2cecab96d6
kokic <3250679033@qq.com>
2023-03-31 19:40:01 +0800

    empty

--- library/markdown.lean
+++ library/markdown.lean
@@ -1,13 +1,2 @@
 
-
-inductive MdElement
-  | heading : Nat → String → MdElement
-  | paragraph : String → MdElement
-  | bold : String → MdElement
-  | italic : String → MdElement
-  | link : String → String → MdElement
-  | image : String → String → MdElement
-
--- 1. implement a function named `markdown`
--- that support all syntax of `MdElement`
--- and output html string
+-- WIP. 
