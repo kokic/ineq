7cbf917c3b2c0e4ff112f50be6e97b58ad5d5b0f
kokic <3250679033@qq.com>
2023-04-16 16:59:33 +0800

    md: paragraph

--- library/markdown.lean
+++ library/markdown.lean
@@ -5,23 +5,28 @@
 import library.parsec
 
 
+def heading := char '#'
 
--- def paragraph := 
+def paragraph := satisfy_alt (λ _ => true) 
+  |>.plus |>.map (s!"<p>{·}</p>")
 
+-- def newline := string "\n\n"
 
+def line (s : String) := 
+  let s := s.replace "\n" ""
+  match paragraph.parse s with
+    | some x => x.fst
+    | none => s
 
+def translate (s : String) := 
+  let xs := s.splitOn s!"\n\n" |>.map line
+  xs.tail!.foldl (λ x y => x ++ "\n" ++ y) (xs.head!)
 
-def elementBegin := char '<' |>.move alphanum |>.skip (char '>')
-def elementEnd := string "</" |>.move alphanum |>.skip (char '>')
+#eval println! translate "
+ab
+dd
 
-
-partial def element _ := 
-  let elementAux := alphanum.eitherLazy element
-  elementBegin.follow (elementAux)
-    |>.follow (elementEnd)
-    |>.filter (λ x => x.fst.fst == x.snd)
-    |>.map (λ x => x.fst.snd)
-
-#eval element () |>.parse "<p><a>text</a></p>"
+gc
+"
 
 
