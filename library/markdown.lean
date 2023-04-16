f7fdf0126abc8f1ca8c98b860793e1b1be4e6176
kokic <3250679033@qq.com>
2023-04-17 00:06:41 +0800

    mardkown prop

--- library/markdown.lean
+++ library/markdown.lean
@@ -5,57 +5,70 @@
 import library.parsec
 
 
-def heading := char_alt '#' |>.plus.map (·.length)
-  |>.follow (satisfy_alt (· != '\n') |>.plus)
-  |>.map (λ (n, s) => s!"<h{n}>{s}</h{n}>")
-
-
-
-def paragraphLine := satisfy_alt (λ _ => true) |>.plus
-
--- def newline := string "\n\n"
-
--- def line (s : String) := 
---   let s := s.trim
---   let p := heading |>.or paragraph
---   match p.parse s with
---     | some x => x.fst
---     | none => s
-
-
--- Dalvik & Zygote
-
-inductive MdElement
+inductive MdElement 
   | heading : Nat → String → MdElement
-  | paraline : String → MdElement
+  | paragraphline : String → MdElement
+  | paragraph : String → MdElement
+  | breakline : MdElement
+
+
   -- | bold : String → MdElement
   -- | italic : String → MdElement
   -- | link : String → String → MdElement
   -- | image : String → String → MdElement
-  
--- def append : MdElement -> MdElement -> MdElement := match (a, b) with
---   | 
---   | 
+  | empty : MdElement
+  | markdown : MdElement → MdElement → MdElement
+
 
 def MdElement.append : (MdElement -> MdElement -> MdElement) 
-  | a, b => a
-
+  | paragraphline a, paragraphline b 
+  | paragraph a, paragraphline b => paragraph (s!"{a} {b}")
+  | markdown x y, a => markdown x (y.append a) 
+  | a, b => markdown a b
 
 instance : Append MdElement := ⟨MdElement.append⟩ 
 
-partial def translate (s : String) := 
-  let source := s.trim.splitOn "\n" 
-  let size := source.length
 
-  let rec translate_aux (pos : Nat) (xs : List String) := 
-    let read n := source[pos + n]!.trim
-    let (curr, peek) := (read 0, read 1)
-    let when (p : Bool) (r : List String) := if p then r else xs
-    if pos >= size then xs else
-    match curr with
-      | "" => translate_aux (pos + 1) (xs ++ ["<br>"])
-      | s => translate_aux (pos + 1) (xs ++ [s])  
-  translate_aux 0 []
+def MdElement.toString : (MdElement -> String) 
+  | heading n s => s!"<h{n}>{s}</h{n}>"
+  | paragraphline s 
+  | paragraph s => s!"<p>{s}</p>"
+  | breakline => "<br>"
+  | markdown x y => x.toString ++ y.toString
+  | _ => ""
+
+instance : ToString MdElement := ⟨MdElement.toString⟩ 
+
+def all := satisfy_alt (λ _ => true) |>.plus
+
+def heading := char_alt '#' |>.plus.map (·.length) 
+  |>.follow all
+  |>.map (λ (n, s) => MdElement.heading n s)
+
+def paragraphLine := all.map MdElement.paragraphline 
+
+def line := heading.or paragraphLine
+
+
+def transmorph : String -> MdElement 
+  | s => s.trim |> line.parse 
+                |>.getD (MdElement.empty, "") 
+                |>.fst
+
+partial def translate (s : String) := 
+  let xs := s.trim.splitOn "\n" |>.map transmorph
+  xs.tail!.foldl MdElement.append (xs.headD MdElement.empty) |>.toString
+
+  -- let size := source.length
+  -- let rec translate_aux (pos : Nat) (xs : List String) := 
+    -- let read n := source[pos + n]!.trim
+    -- let (curr, peek) := (read 0, read 1)
+    -- let when (p : Bool) (r : List String) := if p then r else xs
+    -- if pos >= size then xs else
+    -- match curr with
+      -- | "" => translate_aux (pos + 1) (xs ++ ["<br>"])
+      -- | s => translate_aux (pos + 1) (xs ++ [s])  
+  -- translate_aux 0 []
 
 
 
@@ -66,6 +79,24 @@ def read_file (path : String) : IO String :=
       pure contents 
 
 
-#eval read_file "./library/markdown-in.md"
-  >>= (λ s => s |> translate |> pure)
-  >>= IO.println
+-- #eval read_file "./library/markdown-in.md"
+--   >>= (λ s => s |> translate |> pure)
+--   >>= IO.println
+
+#eval translate "
+
+# Heading 1
+## Heading 2
+
+line 1 and
+same line
+
+line 2 and
+a
+b
+c
+
+Dalvik & Zygote
+
+end
+"
