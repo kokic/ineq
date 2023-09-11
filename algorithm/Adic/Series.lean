bb09011361d0c53f472674d1b513856fbdf28fc5
kokic <3250679033@qq.com>
2023-09-12 02:22:48 +0800

    p-adic sqrt calculate

--- algorithm/Adic/Series.lean
+++ algorithm/Adic/Series.lean
@@ -14,19 +14,24 @@ partial def fromNatAux (adic residue power : Nat)
   match residue with
     | 0 => data
     | r => 
-      let base := adic ^ (power - 1)
-      let aᵢ := r % (adic ^ power) / base
+      let base := adic ^ power
+      let aᵢ := r % adic ^ (power + 1) / base
       let term := aᵢ * base
       fromNatAux adic (r - term) (power + 1) (data ++ [aᵢ])
 
-def fromNat (adic n : Nat) : AdicNumberSeries := ⟨adic, fromNatAux adic n 1 []⟩
+def fromNat (adic n : Nat) : AdicNumberSeries := ⟨adic, fromNatAux adic n 0 []⟩
 
 
-def toNat (series : AdicNumberSeries) :=
-  let rec aux : List Nat → Nat → Nat
-    | [], _ => 0
-    | x :: xs, n => x * series.adic ^ n + aux xs (n + 1)
-  aux series.data 0
+def toNatAux : Nat → List Nat → Nat → Nat
+  | _, [], _ => 0
+  | adic, x :: xs, n => 
+    x * adic ^ n + toNatAux adic xs (n + 1)
+
+def toNat' : Nat → List Nat → Nat 
+  | adic, data => toNatAux adic data 0
+
+def toNat (series : AdicNumberSeries) := 
+  toNat' series.adic series.data
 
 
 -- #eval toNat 
@@ -34,19 +39,32 @@ def toNat (series : AdicNumberSeries) :=
 
 
 
--- √n i.e. x s.t. x² = n
+
+-- √n i.e. x s.t. x² = a
+
+-- def hasSolution
+
+--| a, adic, 1 => [power_go a adic 0 0]
+def sqrtByEnum : Nat → Nat → Nat → List Nat
+  | _, _, 0 => []
+  | a, adic, n + 1 => 
+    let xs := sqrtByEnum a adic n
+    let acc := AdicNumberSeries.toNat' adic xs
+    xs ++ [power_go a adic acc n]
+  where 
+    power_go (a adic acc power : Nat) := 
+      let rec aux : Nat → Nat
+        | 0 => 0 -- no solution
+        | dec + 1 =>
+          let coeff := adic - dec
+          let approx := acc + coeff * adic ^ power
+          if approx ^ 2 % adic ^ (power + 1) == a 
+        then coeff else aux dec
+      aux adic
+
+-- #eval sqrt 5 11 3
 
 
--- def sqrtByEnumAux (n adic power sum : Nat) := Id.run do
---   let mut coeff := 0
---   for i in [ :adic ] do
---     let approx := sum + i * adic ^ power
---     let higher := adic ^ (power + 1)
---     if approx ^ 2 % higher == n then 
---     coeff := i
---   return coeff
-
--- def sqrtByEnum (adic n : Nat) := n
 
 
 
